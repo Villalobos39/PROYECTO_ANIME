32ad7535f4ca7e12fd6b8ada7c3ccbc0f0062a7e
DULCE JASMIN VILLALOBOS PEREZ <dulce.villalobos17@tectijuana.edu.mx>
2022-07-04 18:04:37 -0700

    [INSERTAR TABLA 2 DE ANIME]

--- PROYECTO ANIME/LibreriaAnime/sqlManager/SqlRepository.cs
+++ PROYECTO ANIME/LibreriaAnime/sqlManager/SqlRepository.cs
@@ -83,9 +83,67 @@ namespace AnimeWebApi.sqlManager
                     }
                 }
             }
-
             return anime;
         }
+
+
+        public long Create(Anime anime)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString))
+                {
+                    connection.Open();
+                    var SqlCreate = "Insert into REGISTROS_ANIME_2 (ANIME_ID"
+                            + ",NOMBRE"
+                            + ",GENERO"
+                            + ",TIPO"
+                            + ",EPISODIO"
+                            + " )"
+                            + " Values ( "
+                            + " @ANIME_ID "
+                            + " , @NOMBRE"
+                            + " , @GENERO"
+                            + " , @TIPO "
+                            + " , @EPISODIO) ";
+
+                    SqlCommand sqlCommand = new SqlCommand(SqlCreate, connection);
+
+                    sqlCommand.Parameters.Add(new SqlParameter("@ANIME_ID", anime.Id_Anime));
+                    sqlCommand.Parameters.Add(new SqlParameter("@NOMBRE", anime.Titulo_Anime));
+                    sqlCommand.Parameters.Add(new SqlParameter("@GENERO", anime.Generos));
+                    sqlCommand.Parameters.Add(new SqlParameter("@TIPO", anime.Tipo));
+                    sqlCommand.Parameters.Add(new SqlParameter("@EPISODIO", anime.Episodios));
+
+                    sqlCommand.ExecuteNonQuery();
+                    return anime.Id_Anime;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
     }
 
 }
+
+
+/// ESQUEMA DE LA BASE DE DATOS 
+
+//CREATE TABLE REGISTROS_ANIME_2(
+//	ANIME_ID int,
+//    NOMBRE varchar(255),
+//    GENERO varchar(255),
+//    TIPO varchar(20),
+//    EPISODIO int
+//);
+
+//INSERT INTO REGISTROS_ANIME VALUES (32281, 'Kimi no Na wa.', 'Drama, Romance, School, Supernatural','Movie',1);
+//INSERT INTO REGISTROS_ANIME VALUES (5114, 'Fullmetal Alchemist: Brotherhood', 'Action, Adventure, Drama, Fantasy, Magic, Military, Shounen','TV',64);
+//INSERT INTO REGISTROS_ANIME VALUES (28977, 'Gintamaa.', 'Action, Comedy, Historical, Parody, Samurai, Sci-Fi, Shounen','TV',51);
+//INSERT INTO REGISTROS_ANIME VALUES (21, 'One Piece.', 'Action, Adventure, Comedy, Drama, Fantasy, Shounen, Super Power','TV',1018);
+//INSERT INTO REGISTROS_ANIME VALUES (28755, 'Boruto: Naruto the Movie.', 'Action, Comedy, Martial Arts, Shounen, Super Power','Movie',1);
+//INSERT INTO REGISTROS_ANIME VALUES (1735, 'Naruto: Shippuuden.', 'Action, Comedy, Martial Arts, Shounen, Super Power','TV',500);
